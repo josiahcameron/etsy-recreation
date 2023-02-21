f9d9bd9beba29c297e123ebb9f943bc905641bb1
josiahcameron <josiahcameron2015@gmail.com>
2023-02-20 20:08:16 -0500

    Functioning Handlebars

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
 import data from "./data.js"
 
-// Retrieves template data from html
-const source = document.getElementById("card-item-template").innerHTML;
 
-// Compiles template data into a function
-const template = Handlebars.compile(source);
+(function(){
+    "use strict";
 
-const context = data
+    const source = document.getElementsByClassName("etsy-card-template")[0].innerHTML;
+    const template = Handlebars.compile(source);
 
-const html = template(context);
+    const context = data
+    const html = template(context);
 
-document.querySelector("cards").insertAdjacentHTML("afterbegin", html)
+    document.getElementsByClassName("etsy-list")[0].insertAdjacentHTML("afterbegin", html)
+})();
 
-console.log("Hello World")
 
-function generateHTML(data){
-    
+// // Retrieves template data from html
+// const source = document.getElementById("card-item-template").innerHTML;
 
-}
+// // Compiles template data into a function
+// const template = Handlebars.compile(source);
+
+
+
+// const html = template(context);
+
+// document.querySelector("cards").insertAdjacentHTML("afterbegin", html)
+
+// console.log("Hello World")
+
+
+
